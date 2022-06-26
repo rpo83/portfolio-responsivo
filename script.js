46050b723222c76935ed9b3334e6b7b150dd5d21
Renato Pereira de Oliveira <97848524+rpo83@users.noreply.github.com>
2022-06-26 20:06:46 -0300

    Portfolio pessoal
    Portfolio criado para o curso de desenvolvimento de sistemas no Senai Roberto Simonsen .

---
+++ script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // barra de navegação no script de rolagem
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // botão de rolagem mostrar/ocultar script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // roteiro deslizante
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removendo a rolagem suave ao clicar no botão deslizante
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // aplicando novamente a rolagem suave nos itens do menu clique
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // alternar script de menu/barra de navegação
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // digitando script de animação de texto
+    var typed = new Typed(".typing", {
+        strings: ["Aprendiz Senai Scania", "Desenvolvedor Web", "Estudante de Desenvolvimento de Sistemas", "Gamer de Coração", "Mecatrônico"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Aprendiz Senai Scania", "Desenvolvedor Web", "Estudante de Desenvolvimento de Sistemas", "Gamer de Coração", "Mecatrônico"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // roteiro de carrossel de coruja
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
